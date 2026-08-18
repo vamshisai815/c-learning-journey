5cc62a6805c6a888782ad2782b0f0d54ffedce37
vamshisai815 <vsai16988@gmail.com>
2026-08-18 21:44:14 +0530

    Add C First Program

---
+++ 02_variable_datatypes_Comments_EscapeSequance.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+    // integer variable 
+    int age = 20;
+    // float variable 
+    float height = 6.8;
+    // character Variable 
+    char Grade = 'A';
+
+    printf("Age of the Student is: %d\n",age);
+    printf("Height of the Student is: %f\n" ,height);
+    printf("Grade of the student is : %c\n",Grade);
+    
+
+    // Changing stored value 
+    // intial or first value 
+    int number = 100;
+    printf("The intial value of the number is : %d\n", number);
+
+    // I want to change the value of the inital value to the 25
+    // update value 
+
+    number = 89;
+    printf("The Update Value of the number is : %d\n",number);
+
+    // I want to add expresion in the middle of the update value 
+    number= number + 9090;
+    printf("The Update value using An Expression is : %d\n",number);
+
+
+    // Changing the value using character 
+    // The initial value of the character
+    char charcter = 'Y';
+    printf("The intial value of the Character:%c\n",charcter);
+    return 0;
+}
+    
+
+    // Data Tyoe in c 
+    
+
---
+++ small_program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    //printf("Hello.........\n");
+    printf("--------------------------------\n");
+    printf("MY INTRODUCTION\n");
+    printf("---------------------------------\n");
+    printf("Name: T Vamshi Sai\n");
+    printf("Course: BCA(Hons/Hons with Research\n");
+    printf("Goal: Software Developer Engineer\n");
+    printf("Skill: Learning C Language\n");
+    printf("-----------------------------------\n");
+    printf("MY CODING JOUNERY IS BEGINS\n");
+    printf("------------------------------------\n");
+    return 0;
+}
+
