b39e82b0bb16b949eb5a49858c9bf9a3fcc1a606
vamshisai815 <vsai16988@gmail.com>
2026-08-25 02:03:51 +0530

    Update Practice_set.c

--- Practice_set.c
+++ Practice_set.c
@@ -8,17 +8,17 @@ int main()
     //a) using hard coded inputs
     //b) using input supplied by the user
 
-    // int length ,width;
+     int length ,width;
     
-    // printf("Enter The length 0f the rectangle\n: ");
-    // scanf("%d",&length);
+     printf("Enter The length 0f the rectangle\n: ");
+     scanf("%d",&length);
 
-    // printf("Enter The Width of the rectangle\n: ");
-    // scanf("%d", &width);
+     printf("Enter The Width of the rectangle\n: ");
+     scanf("%d", &width);
 
-    // printf("Length of the Rectangle: %d\n",length);
-    // printf("WIdth of the Rectangle:%d\n", width);
-    // printf("Area of the Rectangle : %d\n", length*width);
+     printf("Length of the Rectangle: %d\n",length);
+     printf("WIdth of the Rectangle:%d\n", width);
+     printf("Area of the Rectangle : %d\n", length*width);
 
 
     //2 Calculate the area of a circle and modify the same program to calcualte the volume of a cylinder given its radius and height 
@@ -29,7 +29,7 @@ int main()
     printf("Area of the circle in the Radius of %d is %f\n",radius,Pi1);
     printf("Volume of the cylinder in the radius %d and the Height %d is %f\n",radius,height,Pi2);
     
-    //3 Write a program to convert celsius (Centigrade) temperature to fahreheit
+    
 
     //4 Write a program to calculate simple interest for a set of values representing principal number of years, princial, years, and rate of interest 
 
